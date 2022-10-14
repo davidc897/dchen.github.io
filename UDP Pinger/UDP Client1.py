1c055b07154f3f593a538720e4359b1e320485e2
davidc897 <56775888+davidc897@users.noreply.github.com>
2022-10-14 02:40:30 -0700

    Update UDP Client1.py

--- UDP Pinger/UDP Client1.py
+++ UDP Pinger/UDP Client1.py
@@ -16,30 +16,28 @@ pings = 0
 # Timeout variable
 timeout = 0
 # Try and Except for timeout
-#clientSocket.settimeout(1)
+clientSocket.settimeout(1)
 # Messages / pings in a for loop of 10 
-while True:
-	#try:
-	pings = pings + 1
-	message = str(pings)
+for pings in range (0,100):
+	try:
+		pings = pings + 1
+		message = str(pings)
 # Send messages or pings
-	time.sleep(5)
-	clientSocket.sendto(message.encode(),(serverName, serverPort))
-		#T1 = time.time()
+		clientSocket.sendto(message.encode(),(serverName, serverPort))
+		T1 = time.time()
 # Receieve respondes from server and capture time elapsed
-	#message2, serverAddress = clientSocket.recvfrom(1024)
-		#T2 = time.time()
-		#Timelist.append(T2-T1)
+		message2, serverAddress = clientSocket.recvfrom(1024)
+		T2 = time.time()
+		Timelist.append(T2-T1)
 # Print received messages
-		#print('Ping ' + str(pings) +': host replied: seq ' + message2.decode() + ' ' + time.asctime() + ", RTT = " + str(round((T2-T1)*1000,2)) + " ms")
-	print("heartbeat pluse " + str(pings))
-	#except: 
-	#print('Request timed out, message was lost')
-	#timeout = timeout + 1
+		IP = serverAddress[0]
+		print('Ping ' + str(pings) +': host ' + str(IP) + ' replied: seq ' + message2.decode() + ' ' + time.asctime() + ", RTT = " + str(round((T2-T1)*1000,2)) + " ms")
+	except: 
+		print('Request timed out, message was lost')
+		timeout = timeout + 1
 # Print Min, Max, Avg values
-#print("Min RTT: " + str(round(min(Timelist)*1000,2)) + ' ms')
-#print("Max RTT: " + str(round(max(Timelist)*1000,2)) + ' ms')
-#print("Avg RTT: " + str(round(sum(Timelist)/len(Timelist)*1000,2)) + ' ms')
-#print("Packet lost: " + str(round(100*(1-((pings-timeout)/pings)),2)) + ' %'  )
+print("Min RTT: " + str(round(min(Timelist)*1000,2)) + ' ms')
+print("Max RTT: " + str(round(max(Timelist)*1000,2)) + ' ms')
+print("Avg RTT: " + str(round(sum(Timelist)/len(Timelist)*1000,2)) + ' ms')
+print("Packet lost: " + str(round(100*(1-((pings-timeout)/pings)),2)) + ' %'  )
 clientSocket.close()
- 
